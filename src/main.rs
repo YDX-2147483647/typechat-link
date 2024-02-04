cf7185664501f0e5a33b2dea9072ed2622d2c1ef
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-02-05 00:08:15 +0800

    feat: Fetch links

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::{self, File},
-    io::Write,
+    io::{Read, Write},
+    thread, time,
 };
 
 use reqwest::blocking::get;
@@ -11,6 +12,12 @@ use serde::{Deserialize, Serialize};
 struct Episode {
     pub name: String,
     pub url: String,
+}
+
+#[derive(Debug, Serialize)]
+struct Link<'a> {
+    pub from_url: &'a str,
+    pub to_url: String,
 }
 
 impl Episode {
@@ -26,6 +33,7 @@ impl Episode {
     }
 }
 
+/// Fetch the catalog of episodes
 fn fetch_catalog() -> Vec<Episode> {
     let document = get("https://www.thetype.com/typechat/")
         .unwrap()
@@ -40,12 +48,64 @@ fn fetch_catalog() -> Vec<Episode> {
         .collect()
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let episodes = fetch_catalog();
+/// Fetch links in an episode's show notes
+fn fetch_episode_detail(episode: &Episode) -> Vec<Link> {
+    let document = get(&episode.url).unwrap().text().unwrap();
+    let document = Html::parse_document(&document);
 
-    fs::create_dir_all("data")?;
-    let mut file = File::create("data/episodes.json")?;
-    file.write_all(serde_json::to_string(&episodes)?.as_bytes())?;
+    let selector = Selector::parse("#content > .typechat > .entry-content a").unwrap();
+    document
+        .select(&selector)
+        .map(|a| {
+            let to_url = a
+                .value()
+                .attr("href")
+                .expect("an anchor should have a href")
+                .to_owned();
+            Link {
+                from_url: &episode.url,
+                to_url,
+            }
+        })
+        .collect()
+}
+
+const DATA_DIR: &str = "data";
+const EPISODES_DATA: &str = "data/episodes.json";
+const LINKS_DATA: &str = "data/links.json";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Load or fetch+save episodes
+    let episodes = if let Ok(mut file) = File::open(EPISODES_DATA) {
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer)?;
+        serde_json::from_str(&buffer).unwrap()
+    } else {
+        let episodes = fetch_catalog();
+
+        fs::create_dir_all(DATA_DIR)?;
+        let mut file = File::create(EPISODES_DATA)?;
+        file.write_all(serde_json::to_string(&episodes)?.as_bytes())?;
+
+        episodes
+    };
+
+    // Fetch links
+    let mut links = Vec::new();
+    for e in &episodes {
+        println!("🚀 Fetching “{}”…", e.name);
+
+        let mut l = fetch_episode_detail(e);
+        println!("✅ Got {} links.", l.len());
+        links.append(&mut l);
+
+        println!("💤 (Sleep for a second)");
+        thread::sleep(time::Duration::from_secs(1));
+    }
+
+    // Save links
+    let mut file = File::create(LINKS_DATA)?;
+    file.write_all(serde_json::to_string(&links)?.as_bytes())?;
 
     Ok(())
 }
